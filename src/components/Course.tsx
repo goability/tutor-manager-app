a2db99c932c803a34840a1ecc555624cc2290c0e
Matt Chandler <matt.chandleraz@gmail.com>
2024-08-24 18:56:47 -0500

    work (#1)

---
+++ src/components/Course.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import '../styles/course.css';
+import CourseContent from './CourseContent';
+
+
+const Course: React.FC = () => {
+
+  
+  //"This is the course body";
+  const params = useParams<'course_name'>();
+  const course_name = params.course_name;
+
+  return (
+    <div className='course_container'>
+      <CourseContent />
+     
+    </div>
+  );
+};
+
+export default Course;
